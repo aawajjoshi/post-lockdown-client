a1db3442c5dc878ccdf95228320e20973ee97985
Aawaj Joshi <s521315@nwmissouri.edu>
2020-07-03 02:21:27 -0500

    edit proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:3000'
+    uri: 'https://postlockdown.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
